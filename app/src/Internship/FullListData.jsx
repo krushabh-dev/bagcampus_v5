56d0e8b8a3a5e4bc578397f8371dc161bcc18598
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2021-04-18 19:46:55 +0530

    Create FullListData.jsx

---
+++ app/src/Internship/FullListData.jsx
@@ -0,0 +1,311 @@
+const FullListData = [
+  //id : 01
+
+  {
+    id: "01",
+    "@context": "https://schema.org/",
+    "@type": "JobPosting",
+    title: "Software Development Engineer Intern",
+    type: "Internship",
+    typeTag: "part-time",
+    description:
+      "Amazon is hiring the best minds in technology to innovate and build on behalf of our customers",
+    identifier: {
+      "@type": "PropertyValue",
+      name: "Amazon",
+      value: "1234567",
+    },
+    datePosted: "2021-04-13",
+    validThrough: "2021-05-13T00:00",
+    Location: "Bengaluru",
+    ApplyLink:
+      "https://www.amazon.jobs/en/jobs/1227693/software-development-engineer-intern",
+    employmentType: "CONTRACTOR",
+    hiringOrganization: {
+      "@type": "Organization",
+      name: "Amazon",
+      sameAs: "http://www.amazon.jobs",
+      logo:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1000px-Amazon_logo.svg.png",
+    },
+    jobLocation: {
+      "@type": "Place",
+      address: {
+        "@type": "PostalAddress",
+        streetAddress: "2nd Floor, Safina Towers",
+        addressLocality: "2nd Floor, Safina Towers",
+        addressRegion: "KA",
+        postalCode: "560052",
+        addressCountry: "IN",
+      },
+    },
+    baseSalary: {
+      "@type": "MonetaryAmount",
+      currency: "USD",
+      value: {
+        "@type": "QuantitativeValue",
+        value: undefined,
+        unitText: "HOUR",
+      },
+    },
+  },
+
+  //id : 02
+
+  {
+    id: "02",
+    "@context": "https://schema.org/",
+    "@type": "JobPosting",
+    title: "Research Intern",
+    type: "Internship",
+    typeTag: "part-time",
+    description:
+      "Microsoft is Hiring Intern To Work with some of the world’s best researchers, interns learn, collaborate, and network for life",
+    identifier: {
+      "@type": "PropertyValue",
+      name: "Microsoft",
+      value: "1234567",
+    },
+    datePosted: "2021-04-13",
+    validThrough: "2021-05-13T00:00",
+    Location: "Bangalore",
+    ApplyLink:
+      "https://careers.microsoft.com/us/en/job/931311/Research-Intern/",
+    employmentType: "CONTRACTOR",
+    hiringOrganization: {
+      "@type": "Organization",
+      name: "Microsoft",
+      sameAs: "https://careers.microsoft.com/",
+      logo:
+        "https://prodcmscdn.azureedge.net/careerconnectresources/p/MICRUS/en_us/desktop/assets/images/microsoft-gray.png",
+    },
+    jobLocation: {
+      "@type": "Place",
+      address: {
+        "@type": "PostalAddress",
+        streetAddress:
+          "4th Floor, Silver Oak, Manyata Embassy Business Park, Outer Ring Road",
+        addressLocality: "Bangalore",
+        addressRegion: "KA",
+        postalCode: "560045",
+        addressCountry: "IN",
+      },
+    },
+    baseSalary: {
+      "@type": "MonetaryAmount",
+      currency: "USD",
+      value: {
+        "@type": "QuantitativeValue",
+        value: undefined,
+        unitText: "HOUR",
+      },
+    },
+  },
+
+  //id : 03
+
+  {
+    id: "04",
+    "@context": "https://schema.org/",
+    "@type": "JobPosting",
+    title: "Graduate Intern Technical",
+    type: "Internship",
+    typeTag: "part-time",
+    description:
+      "Intel is Hiring Interns To involve in the training, design and development of next generation Server SOCs/CPUs",
+    identifier: {
+      "@type": "PropertyValue",
+      name: "Intel",
+      value: "1234567",
+    },
+    datePosted: "2021-04-13",
+    validThrough: "2021-05-13T00:00",
+    Location: "Bangalore",
+    ApplyLink:
+      "https://jobs.intel.com/ShowJob/Id/2388231/Graduate-Intern-Technical",
+    employmentType: "CONTRACTOR",
+    hiringOrganization: {
+      "@type": "Organization",
+      name: "Intel",
+      sameAs: "https://jobs.intel.com/",
+      logo:
+        "https://jobs.intel.com/Views/Client/d1fbe68e-ee7a-4d07-ba71-10c3e62672ab/images/intel-logo-black.png",
+    },
+    jobLocation: {
+      "@type": "Place",
+      address: {
+        "@type": "PostalAddress",
+        streetAddress: "Intel Technology India Pvt. Ltd. - SRR 3",
+        addressLocality: "Outer Ring Road, Varthur Hobli, Bellandur, Bangalore",
+        addressRegion: "KA",
+        postalCode: "560103",
+        addressCountry: "IN",
+      },
+    },
+    baseSalary: {
+      "@type": "MonetaryAmount",
+      currency: "USD",
+      value: {
+        "@type": "QuantitativeValue",
+        value: undefined,
+        unitText: "HOUR",
+      },
+    },
+  },
+
+  //id : 04
+
+  {
+    id: "04",
+    "@context": "https://schema.org/",
+    "@type": "JobPosting",
+    title: "Field Sales Engineer (Intern)",
+    type: "Internship",
+    typeTag: "part-time",
+    description:
+      "TThe Internship Program is specifically designed for university juniors and offers the opportunity to gain experience and grow within Cisco",
+    identifier: {
+      "@type": "PropertyValue",
+      name: "Cisco",
+      value: "1234567",
+    },
+    datePosted: "2020-04-13",
+    validThrough: "2020-05-13T00:00",
+    Location: "Bangalore",
+    ApplyLink:
+      "https://jobs.cisco.com/jobs/SearchJobs/Intern?21178=%5B207928%5D&21178_format=6020&21180=%5B165%5D&21180_format=6022&listFilterMode=1",
+    employmentType: "CONTRACTOR",
+    hiringOrganization: {
+      "@type": "Organization",
+      name: "Cisco",
+      sameAs: "https://jobs.cisco.com/",
+      logo:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Cisco_logo_blue_2016.svg/220px-Cisco_logo_blue_2016.svg.png",
+    },
+    jobLocation: {
+      "@type": "Place",
+      address: {
+        "@type": "PostalAddress",
+        streetAddress:
+          "SEZ Unit, Cessna Business Park, Kadubeesanahalli Village, Hobli, Sarjapur",
+        addressLocality: "Marathahalli, Bengaluru",
+        addressRegion: "KA",
+        postalCode: "560103",
+        addressCountry: "IN",
+      },
+    },
+    baseSalary: {
+      "@type": "MonetaryAmount",
+      currency: "USD",
+      value: {
+        "@type": "QuantitativeValue",
+        value: undefined,
+        unitText: "HOUR",
+      },
+    },
+  },
+
+  //id : 05
+
+  {
+    id: "05",
+    "@context": "https://schema.org/",
+    "@type": "JobPosting",
+    title: "Intern",
+    type: "Internship",
+    typeTag: "part-time",
+    description:
+      "The Intern is responsible for configuring and testing Wipro proprietary Total Benefit Administration (TBA) System to meet the needs of our clients’ requirements",
+    identifier: {
+      "@type": "PropertyValue",
+      name: "Wipro",
+      value: "1234567",
+    },
+    datePosted: "2020-04-09",
+    validThrough: "2020-05-13T00:00",
+    Location: "Chennai",
+    ApplyLink:
+      "https://careers.wipro.com/careers-home/jobs/2623324?lang=en-us&previousLocale=en-US",
+    employmentType: "CONTRACTOR",
+    hiringOrganization: {
+      "@type": "Organization",
+      name: "Wipro",
+      sameAs: "https://careers.wipro.com/",
+      logo:
+        "https://cms.jibecdn.com/prod/wipro/assets/HEADER-NAV_LOGO-en-us-1583314680756.png",
+    },
+    jobLocation: {
+      "@type": "Place",
+      address: {
+        "@type": "PostalAddress",
+        streetAddress:
+          "SEZ Unit, Cessna Business Park, Kadubeesanahalli Village, Hobli, Sarjapur",
+        addressLocality: "Marathahalli, Bengaluru",
+        addressRegion: "KA",
+        postalCode: "560103",
+        addressCountry: "IN",
+      },
+    },
+    baseSalary: {
+      "@type": "MonetaryAmount",
+      currency: "USD",
+      value: {
+        "@type": "QuantitativeValue",
+        value: undefined,
+        unitText: "HOUR",
+      },
+    },
+  },
+
+  //id : 05
+
+  {
+    id: "05",
+    "@context": "https://schema.org/",
+    "@type": "JobPosting",
+    title: "Software Intern",
+    type: "Internship",
+    typeTag: "part-time",
+    description:
+      "Bluetooth and WiFi Systems Connectivity Integration and Validation Engineer Debugs software products through the use of systematic tests to develop apply and maintain quality standards for company products Develops",
+    identifier: {
+      "@type": "PropertyValue",
+      name: "Intel",
+      value: "1234567",
+    },
+    datePosted: "2021-04-15",
+    validThrough: "2021-05-13T00:00",
+    Location: "Bangalore",
+    ApplyLink: "https://jobs.intel.com/ShowJob/Id/2782187/Software%20Intern",
+    employmentType: "CONTRACTOR",
+    hiringOrganization: {
+      "@type": "Organization",
+      name: "Intel",
+      sameAs: "https://jobs.intel.com/",
+      logo:
+        "https://jobs.intel.com/Views/Client/d1fbe68e-ee7a-4d07-ba71-10c3e62672ab/images/intel-logo-black.png",
+    },
+    jobLocation: {
+      "@type": "Place",
+      address: {
+        "@type": "PostalAddress",
+        streetAddress: "Intel Technology India Pvt. Ltd. - SRR 3",
+        addressLocality: "Outer Ring Road, Varthur Hobli, Bellandur, Bangalore",
+        addressRegion: "KA",
+        postalCode: "560103",
+        addressCountry: "IN",
+      },
+    },
+    baseSalary: {
+      "@type": "MonetaryAmount",
+      currency: "USD",
+      value: {
+        "@type": "QuantitativeValue",
+        value: undefined,
+        unitText: "HOUR",
+      },
+    },
+  },
+];
+
+export default FullListData;
