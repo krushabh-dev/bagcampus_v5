350648c455d7faf4d165fe2b030ebdddb1c560b1
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2021-04-25 16:38:31 +0530

    Update SignUp.jsx

--- app/src/SignUp/SignUp.jsx
+++ app/src/SignUp/SignUp.jsx
@@ -92,10 +92,10 @@ const SignInBtn = () => {
 const GForm = () => {
   return (
     <>
-      <form id="login" className="col-md-12">
+      <form className="col-md-12" id="login">
         <div className="form-group col-md-6"></div>
         <div className="car col-md-6">
-          <button type="submit" className="btn btn-primary">
+          <button type="button" class="btn btn-primary" id="liveToastBtn">
             Register With Phone
           </button>
         </div>
@@ -137,6 +137,32 @@ const SignIn = () => {
           <Footer />
         </div>
       </section>
+
+      <div class="position-fixed bottom-0 right-0 p-3 toast-main">
+        <div
+          id="liveToast"
+          class="toast hide"
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
+          data-delay="2000"
+        >
+          <div class="toast-header">
+            <img src="..." class="rounded mr-2" alt="..." />
+            <strong class="mr-auto">Bootstrap</strong>
+            <small>11 mins ago</small>
+            <button
+              type="button"
+              class="ml-2 mb-1 close"
+              data-dismiss="toast"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="toast-body">Hello, world! This is a toast message.</div>
+        </div>
+      </div>
     </>
   );
 };
